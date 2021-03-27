4399468c8bb0a934b558acaee152ef0fb0718799
NicolasOsmundo <nicolasosmundo@hotmail.com>
2021-03-27 02:11:21 -0300

    Adicionar arquivos de projeto.

---
+++ Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ListCS
+{
+    public class Atividades
+    {
+        static void Main(string[] args)
+        {
+            List<Program> lista = new List<Program>();
+        }
+    }
+}
+
+    
+       
+    
+
+
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Program
+{
+
+
+	static void Main(string[] args)
+	{
+		// Exemplo 1
+		string[] nomes1 = { "João", "Maria", "José" };
+		// Exemplo 2
+		string[] nomes2 = new string[3];
+		nomes1[0] = "João";
+		nomes2[1] = "Maria";
+		nomes2[3] = "José";
+	}
+}	
+
